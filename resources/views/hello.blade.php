13bb378d059e0df90561f2f4405b3ae2653b04c5
tomozou-miyake <tomoki2802@gmail.com>
2019-05-18 23:44:50 +0900

    Add lavelapp

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>My First Page</title>
+</head>
+<body>
+  <h2>Hello World!!</h2>
+  <p>皆さん、こんにちは</p>
+Jun/04/2018<p />
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('hello', 'HelloController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
