b5fca2cc227443adcbbe25ca740095b86e1e84a9
tomozou-miyake <tomoki2802@gmail.com>
2019-05-22 02:38:02 +0900

    Add sub_view

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,23 +1,22 @@
-<html>
-<head>
-  <title>HELLO/INDEX</title>
-  <style>
-  body {font-size:16px; color:#999; }
-  h1 { font-size:50px; text-align:right; color:#f6f6f6;
-       margin:-20px 0px -30px 0px; letter-spacing:-4px; }
-  </style>
-</head>
-<body>
-  <h1>Blade/Index</h1>
-  <p>&#064;foreachディレクティブの例</p>
-  @foreach ($data as $item)
-  @if ($loop->first)
-  <p>※データ一覧</p><ul>
-  @endif
-  <li>No,{{$loop->iteration}}. {{$item}}</li>
-  @if ($loop->last)
-  </ul><p>――ここまで</p>
-  @endif
-  @endforeach
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+   @parent
+   インデックスページ
+@endsection
+
+@section('content')
+   <p>ここが本文のコンテンツです。</p>
+   <p>必要なだけ記述できます。</p>
+    @include('components.message', ['msg_title'=>'OK',
+      'msg_content'=>'サブビューです。'])
+@endsection
+
+@section('footer')
+copyright 2017 tuyano.
+@endsection
+  
+
+
