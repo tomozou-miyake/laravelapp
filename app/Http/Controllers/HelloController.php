f073d7b5415ae012f19413d387d0333527169daa
tomozou-miyake <tomoki2802@gmail.com>
2019-05-27 04:38:51 +0900

    Edit database

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
 use Validator;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
   
-   public function index(Request $request)
-   {
-       return view('hello.index', ['msg'=>'フォームを入力下さい。']);
-   }
+    public function index(Request $request)
+    {
+       $items = DB::select('select * from people');
+       return view('hello.index', ['items' => $items]);
+    }
 
-   public function post(HelloRequest $request)
-   {
-       return view('hello.index', ['msg'=>'正しく入力されました！']);
-   }
+//    public function post(HelloRequest $request)
+//    {
+//     //    return view('hello.index', ['msg'=>'正しく入力されました！']);
+//    }
 
 }
