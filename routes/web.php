7b41ae0a3ce1a2224a215e75eeb65caabbd9fa83
tomozou-miyake <tomoki2802@gmail.com>
2019-05-30 21:53:59 +0900

    Add EditAction & UpdateAction

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,12 @@ Route::get('/', function () {
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
 
+Route::get('hello/add', 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
+
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+
 #Route::get('/home', 'HomeController@index')->name('home');
 
 
