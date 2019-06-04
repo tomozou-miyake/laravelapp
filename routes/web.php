b17e34dc2c42e7125ad9086ee1286e0e23c9e486
tomozou-miyake <tomoki2802@gmail.com>
2019-06-04 23:27:54 +0900

    Add delete_action

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::post('hello/add', 'HelloController@create');
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
 
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
+
+
 #Route::get('/home', 'HomeController@index')->name('home');
 
 
