cafe361342bd0ecfbd4ab675cbf2a21d3ce91732
tomozou-miyake <tomoki2802@gmail.com>
2019-06-05 23:43:59 +0900

    Change index_action

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,8 +11,8 @@ class HelloController extends Controller
   
     public function index(Request $request)
     {
-        $items = DB::select('select * from people');
-        return view('hello.index', ['items' => $items]);
+       $items = DB::table('people')->get();
+       return view('hello.index', ['items' => $items]);
     }
     public function post(Request $request)
     {
