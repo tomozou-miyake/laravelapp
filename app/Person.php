b89b956b4f61c80e03698d3dee2d61bd65c03189
tomozou-miyake <tomoki2802@gmail.com>
2019-06-12 12:44:33 +0900

    Add person_edit

--- app/Person.php
+++ app/Person.php
@@ -3,10 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
-    //
     public function getData()
     {
        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
@@ -14,11 +15,29 @@ class Person extends Model
 
     public function scopeNameEqual($query, $str)
     {
-        return $query->where('name', $str);
+       return $query->where('name', $str);
     }
 
     public function scopeAgeGreaterThan($query, $n)
     {
-        return $query->where('age', '<=', $n);
+       return $query->where('age','>=', $n);
     }
+    
+    public function scopeAgeLessThan($query, $n)
+    {
+       return $query->where('age', '<=', $n);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new ScopePerson);
+    }
+    protected $guarded = array('id');
+
+    public static $rules = array(
+       'name' => 'required',
+       'mail' => 'email',
+       'age' => 'integer|min:0|max:150'
+    );
 }
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,13 @@ Route::get('person', 'PersonController@index');
 
 Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
+
+Route::get('person/add', 'PersonController@add');
+Route::post('person/add', 'PersonController@create');
+
+Route::get('person/edit', 'PersonController@edit');
+Route::post('person/edit', 'PersonController@update');
+
 #Route::get('/home', 'HomeController@index')->name('home');
 
 
