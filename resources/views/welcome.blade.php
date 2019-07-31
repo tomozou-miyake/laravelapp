ddc80ca06255d87a275916a202724ff65e6a4884
tomozou-miyake <tomoki2802@gmail.com>
2019-07-31 21:03:01 +0900

    Delete comment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,14 +96,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="{{ url('/hello') }}">Portfolio</a>
-                    <!-- <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
+                    <a href="{{ url('/hello') }}">Enter</a>
                 </div>
             </div>
         </div>
